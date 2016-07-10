c74fd52c10c7f630cf02b09e9e22d6f152970880
Serge Sukhanov <ssuhanov@icloud.com>
2016-07-10 19:48:29 +0300

    Done some code refactor

--- MultiThreadingSlider/ViewController.swift
+++ MultiThreadingSlider/ViewController.swift
@@ -15,62 +15,42 @@ class ViewController: UIViewController {
     
     var currentValue: Float = 50
 
+    func moveSliderRight() {
+        self.currentValue += 0.01
+        print("+++ \(self.currentValue)")
+        
+        dispatch_async(dispatch_get_main_queue(), {
+            self.outletSlider.value = self.currentValue
+            self.outletLabel.text = String(format: "%.2f", self.currentValue)
+        })
+        
+    }
+    
+    func moveSliderLeft() {
+        self.currentValue -= 0.01
+        print("--- \(self.currentValue)")
+        
+        dispatch_async(dispatch_get_main_queue(), {
+            self.outletSlider.value = self.currentValue
+            self.outletLabel.text = String(format: "%.2f", self.currentValue)
+        })
+    }
+    
+    func startQueue(queueName: String, moveFunction: () -> ()) {
+        dispatch_async(dispatch_queue_create(queueName, DISPATCH_QUEUE_CONCURRENT)) {
+            while self.currentValue > 0.01 && self.currentValue < 99.99 {
+                moveFunction()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let mainQ = dispatch_get_main_queue()
-        let queueForIncrement1 = dispatch_queue_create("queueForIncrement1", DISPATCH_QUEUE_CONCURRENT)
-        let queueForIncrement2 = dispatch_queue_create("queueForIncrement2", DISPATCH_QUEUE_CONCURRENT)
-        let queueForDecrement1 = dispatch_queue_create("queueForDecrement1", DISPATCH_QUEUE_CONCURRENT)
-        let queueForDecrement2 = dispatch_queue_create("queueForDecrement2", DISPATCH_QUEUE_CONCURRENT)
-        
-        dispatch_async(queueForIncrement1) {
-            while self.currentValue > 0.01 && self.currentValue < 99.99 {
-                self.currentValue += 0.01
-                print("+++ \(self.currentValue)")
-                
-                dispatch_async(mainQ, {
-                    self.outletSlider.value = self.currentValue
-                    self.outletLabel.text = String(format: "%.2f", self.currentValue)
-                })
-            }
-        }
-        
-        dispatch_async(queueForDecrement1) {
-            while self.currentValue > 0.01 && self.currentValue < 99.99 {
-                self.currentValue -= 0.01
-                print("--- \(self.currentValue)")
-                
-                dispatch_async(mainQ, {
-                    self.outletSlider.value = self.currentValue
-                    self.outletLabel.text = String(format: "%.2f", self.currentValue)
-                })
-            }
-        }
-        
-        dispatch_async(queueForDecrement2) {
-            while self.currentValue > 0.01 && self.currentValue < 99.99 {
-                self.currentValue -= 0.01
-                print("--- \(self.currentValue)")
-                
-                dispatch_async(mainQ, {
-                    self.outletSlider.value = self.currentValue
-                    self.outletLabel.text = String(format: "%.2f", self.currentValue)
-                })
-            }
-        }
-        
-        dispatch_async(queueForIncrement2) {
-            while self.currentValue > 0.01 && self.currentValue < 99.99 {
-                self.currentValue += 0.01
-                print("+++ \(self.currentValue)")
-                
-                dispatch_async(mainQ, {
-                    self.outletSlider.value = self.currentValue
-                    self.outletLabel.text = String(format: "%.2f", self.currentValue)
-                })
-            }
-        }
+        startQueue("queueForIncrement1", moveFunction: moveSliderRight)
+        startQueue("queueForDecrement1", moveFunction: moveSliderLeft)
+        startQueue("queueForDecrement2", moveFunction: moveSliderLeft)
+        startQueue("queueForIncrement2", moveFunction: moveSliderRight)
     }
 
 }
