71496c50de0b09f609564c5e5931e2c72e62d997
Serge Sukhanov <ssuhanov@icloud.com>
2016-07-16 16:47:00 +0300

    Added method for UI changes

--- MultiThreadingSlider/ViewController.swift
+++ MultiThreadingSlider/ViewController.swift
@@ -15,16 +15,19 @@ class ViewController: UIViewController {
     
     var currentValue: Float = 50
     
+    func changeUI() {
+        dispatch_async(dispatch_get_main_queue()) {
+            self.outletSlider.value = self.currentValue
+            self.outletLabel.text = String(format: "%.2f", self.currentValue)
+        }
+    }
+    
     func startQueue(queueName: String, value: Float) {
         dispatch_async(dispatch_queue_create(queueName, DISPATCH_QUEUE_CONCURRENT)) {
             while self.currentValue > 0.01 && self.currentValue < 99.99 {
                 self.currentValue += value
                 print((value>0 ? "+++" : "---") + " \(self.currentValue)")
-                
-                dispatch_async(dispatch_get_main_queue()) {
-                    self.outletSlider.value = self.currentValue
-                    self.outletLabel.text = String(format: "%.2f", self.currentValue)
-                }
+                self.changeUI()
             }
         }
     }
